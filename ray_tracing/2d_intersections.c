beb2f0944b94b481b4fc5f94254b4138a99cc4d7
sdrewdle <sdrew@lclark.edu>
2018-03-07 21:17:18 -0800

    should change y=mx+b eqn to vector: solves directionality and vertical issues

--- ray_tracing/2d_intersections.c
+++ ray_tracing/2d_intersections.c
@@ -1,6 +1,12 @@
 #include <FPT.h>
 #include <D3d_matrix.h>
 
+#define BG 0.1
+#define X 0
+#define Y 1
+
+int MN;
+
 double sgn(double v)
 {
   if (v > 0) return  1.0  ;
@@ -8,17 +14,28 @@ double sgn(double v)
   return 0.0 ;
 }
 
+void gather_points(double p1[2], double p2[2]) {
+  G_wait_click(p1);
+  if(p1[0]<=5 && p1[1]<=5){exit(1);}
+  G_rgb(.7,.8,.9);
+  G_fill_circle(p1[0],p1[1],2);
+
+  G_wait_click(p2);
+  if(p2[0]<=5 && p2[1]<=5){exit(1);}
+  G_fill_circle(p2[0],p2[1],2);
+}
+
 // circle :   x^2 + y^2 = 1
 int f1 (double u, double xy[2])
 {
   xy[0] = cos(u) ;  
   xy[1] = sin(u) ;
+  return 1;
 }
 
 int plot (double ulo, double uhi,
           int (*func)(double u, double xy[2]),
-          double mat[4][4]
-	 ) 
+          double mat[4][4]) 
 {
   double u,p[3] ;
   double twovals[2] ;
@@ -29,126 +46,207 @@ int plot (double ulo, double uhi,
     D3d_mat_mult_pt(p,mat,p) ;
     G_point(p[0],p[1]) ;
   }
+  return 1;
 }
 
-// n = (b-a) x (c-a)
-int plot_normals(double ulo, double uhi,
-          int (*func)(double u, double xy[2]),
-          double mat[4][4]
-	 ) 
-{
-  double u,p[3] ;
-  double twovals[2] ;
-  double twovalsb[2];
+void draw_circles(double m[9][4][4], int mn) {
+  double c[3] = {0,0,0};
+  char num[4];
+  int i;
+  G_rgb(0,1,1);
+  for(i=0; i<mn; i++) {
+    c[0] = 0; c[1] = 0; c[2] = 0;
+    plot(0,2*M_PI, f1, m[i]);
+    D3d_mat_mult_pt(c,m[i],c);
 
-  double a,b;
-  double xx,yy;
-
-  
-  for (u = ulo ; u <= uhi ; u += 0.1) {
-    func(u, twovals) ;
-    func((u-0.1), twovalsb);
-    D3d_mat_mult_pt(twovals,mat,twovals);
-    D3d_mat_mult_pt(twovalsb,mat,twovalsb);
-
-    
-    a = twovalsb[0] - twovals[0];
-    b = twovalsb[1] - twovals[1];
-
-    xx = twovals[0] - b;
-    yy = twovals[1] + a;
-    
-    G_line(twovals[0],twovals[1],xx,yy) ;
+    sprintf(num, "%d", i);
+    G_draw_string(num,c[0]-6,c[1]-6);
   }
 }
 
+void compute_best_intersection(double mat[9][4][4], double imat[9][4][4],
+                               double p1[3], double p2[3],
+                               double ui[2]) {
+  double slope, inter, a, b, c, pos[3], neg[3], ltemp;
+  double pa[3]; // = {p1[0], p1[1], 0};
+  double pb[3]; // = {p2[0], p2[1], 0};
+
+  ui[1] = -1;
+  double shortest_length = 10000;
+  int i;
+
+  for(i=0;i<MN; i++) {
+    //p1[0] = P1[0]; p1[1] = P1[1]; p1[2] = P1[2];
+    //p2[0] = P2[0]; p2[1] = P2[1]; p2[2] = P2[2];
 
 
+    D3d_mat_mult_pt(pa,imat[i],p1);
+    D3d_mat_mult_pt(pb,imat[i],p2);
 
+    // finding equation of the line y=mx+b
+    slope = (pb[1]-pa[1]) / (pb[0]-pa[0]);
+    inter = pa[1]  - slope*pa[0];
+
+    // finding parameters of quadratic equation
+    a = slope*slope + 1;
+    b = 2 * slope * inter;
+    c = inter*inter - 1;
+
+    // solving quadratic equation
+    pos[0] = (-b + sqrt(b*b - 4*a*c)) / (2 * a);
+    neg[0] = (-b - sqrt(b*b - 4*a*c)) / (2 * a);
+    //check for nan
+    if(isnan(pos[0]) && isnan(neg[0])){continue;}
+
+    pos[1] = slope * pos[0] + inter;
+    neg[1] = slope * neg[0] + inter;
+
+
+    // back in normal view 
+    D3d_mat_mult_pt(pos,mat[i],pos);
+    D3d_mat_mult_pt(neg,mat[i],neg);
+
+    ltemp = sqrt(pow(pos[0]-p2[0],2) + pow(pos[1]-p2[1],2));
+    if(ltemp < shortest_length){
+      D3d_mat_mult_pt(pos,imat[i],pos);
+      ui[0] = atan2(pos[1],pos[0]);
+      ui[1] = i;
+      shortest_length = ltemp;
+
+      D3d_mat_mult_pt(pos,mat[i],pos);
+    }
+
+    ltemp = sqrt(pow(neg[0]-p2[0],2) + pow(neg[1]-p2[1],2));
+    if (ltemp < shortest_length){
+      D3d_mat_mult_pt(neg,imat[i],neg);
+      ui[0] = atan2(neg[1],neg[0]);
+      ui[1] = i;
+      shortest_length = ltemp;
+
+      D3d_mat_mult_pt(neg,mat[i],neg);
+    }
+
+  }
+}
 
 int main()
 {
-  int i, Tn, Ttypelist[100] ;
+  int Tn, Ttypelist[100] ;
   double Tvlist[100] ;
-  double mat[4][4],imat[4][4] ;
+  int mn = 0;
+  double mat[9][4][4],imat[9][4][4] ;
 
   G_init_graphics(800,800) ;
   G_rgb(0.1, 0.1, 0.1) ;
   G_clear() ;
 
   //---------------------------------------------------------
-  // circle
+  // circle 1
   Tn = 0 ; // number of transformations
   Ttypelist[Tn] = SX ; Tvlist[Tn] =   50.0 ; Tn++ ;
-  Ttypelist[Tn] = SY ; Tvlist[Tn] =  100.0 ; Tn++ ;
+  Ttypelist[Tn] = SY ; Tvlist[Tn] =   70.0 ; Tn++ ;
   Ttypelist[Tn] = TX ; Tvlist[Tn] =  300.0 ; Tn++ ;
   Ttypelist[Tn] = TY ; Tvlist[Tn] =  500.0 ; Tn++ ;
-  D3d_make_movement_sequence_matrix (mat,imat,
+  D3d_make_movement_sequence_matrix (mat[mn],imat[mn],
                                      Tn,
                                      Ttypelist,
                                      Tvlist) ;
-  G_rgb(.1,.7,.9) ;
-  plot(0, 2*M_PI,  f1, mat) ;
+  mn++;
+  //---------------------------------------------------------
+  // circle 2
+  Tn = 0 ; // number of transformations
+  Ttypelist[Tn] = SX ; Tvlist[Tn] =   70.0 ; Tn++ ;
+  Ttypelist[Tn] = SY ; Tvlist[Tn] =   20.0 ; Tn++ ;
+  Ttypelist[Tn] = RZ ; Tvlist[Tn] =   80.0 ; Tn++ ;
+  Ttypelist[Tn] = TX ; Tvlist[Tn] =  100.0 ; Tn++ ;
+  Ttypelist[Tn] = TY ; Tvlist[Tn] =  650.0 ; Tn++ ;
+  D3d_make_movement_sequence_matrix (mat[mn],imat[mn],
+                                     Tn,
+                                     Ttypelist,
+                                     Tvlist) ;
+  mn++;
+  //---------------------------------------------------------
+  // circle 3
+  Tn = 0 ; // number of transformations
+  Ttypelist[Tn] = SX ; Tvlist[Tn] =   50.0 ; Tn++ ;
+  Ttypelist[Tn] = SY ; Tvlist[Tn] =  100.0 ; Tn++ ;
+  Ttypelist[Tn] = TX ; Tvlist[Tn] =  600.0 ; Tn++ ;
+  Ttypelist[Tn] = TY ; Tvlist[Tn] =  650.0 ; Tn++ ;
+  D3d_make_movement_sequence_matrix (mat[mn],imat[mn],
+                                     Tn,
+                                     Ttypelist,
+                                     Tvlist) ;
+  mn++;
+  //---------------------------------------------------------
+  // circle 4
+  Tn = 0 ; // number of transformations
+  Ttypelist[Tn] = SX ; Tvlist[Tn] =  150.0 ; Tn++ ;
+  Ttypelist[Tn] = SY ; Tvlist[Tn] =   40.0 ; Tn++ ;
+  Ttypelist[Tn] = RZ ; Tvlist[Tn] =  -20.0 ; Tn++ ;
+  Ttypelist[Tn] = TX ; Tvlist[Tn] =  600.0 ; Tn++ ;
+  Ttypelist[Tn] = TY ; Tvlist[Tn] =  150.0 ; Tn++ ;
+  D3d_make_movement_sequence_matrix (mat[mn],imat[mn],
+                                     Tn,
+                                     Ttypelist,
+                                     Tvlist) ;
+  mn++;
+  //---------------------------------------------------------
+  // circle 5
+  Tn = 0 ; // number of transformations
+  Ttypelist[Tn] = SX ; Tvlist[Tn] =   20.0 ; Tn++ ;
+  Ttypelist[Tn] = SY ; Tvlist[Tn] =   40.0 ; Tn++ ;
+  Ttypelist[Tn] = TX ; Tvlist[Tn] =  100.0 ; Tn++ ;
+  Ttypelist[Tn] = TY ; Tvlist[Tn] =  100.0 ; Tn++ ;
+  D3d_make_movement_sequence_matrix (mat[mn],imat[mn],
+                                     Tn,
+                                     Ttypelist,
+                                     Tvlist) ;
+  mn++;
+  //---------------------------------------------------------
+  MN = mn;
+  draw_circles(mat,mn);
+  //---------------------------------------------------------
 
-  double p1[3], p2[3];
-  G_rgb(1,1,1);
-  
-  G_wait_click(p1);
-  G_fill_circle(p1[0],p1[1],3);
+  G_rgb(1,.4,.4);
+  G_line(0,5,5,5); G_line(5,5,5,0);
+  double p1[3], p2[3],
+    ui_pair[2] = {0,0},
+    ci[3], //current intersection
+    adj[3], // point adjacent to uth
+    u;
 
-  double x_old = p1[0];
-  double y_old = p1[1];
+  int i;
+  while(1) {
+    gather_points(p1,p2);
 
-  G_wait_click(p2);
-  G_fill_circle(p2[0],p2[1],3);
+    compute_best_intersection(mat, imat, p1,p2, ui_pair);
 
-  D3d_mat_mult_pt(p1,imat,p1);  //translate back into simpler space
-  D3d_mat_mult_pt(p2,imat,p2);
+    u = ui_pair[0];
+    i = ui_pair[1];
+    if(i<0){
+      G_rgb(BG,BG,BG);
+      G_fill_circle(p1[X], p1[Y], 3);
+      G_fill_circle(p2[X], p2[Y], 3);
+      continue;
+    }
 
-  double M = (p2[1]-p1[1]) / (p2[0]-p1[0]);
-  double B = p1[1]  - M*p1[0];
-  double a = M*M + 1;
-  double b = 2 * M * B;
-  double c = B*B - 1;
+    ci[X] = cos(u);
+    ci[Y] = sin(u);
+    D3d_mat_mult_pt(ci,mat[i],ci);
 
-  double pos = (-b + sqrt(b*b - 4*a*c)) / (2 * a);
-  double neg = (-b - sqrt(b*b - 4*a*c)) / (2 * a);
-  double x,y, xx,yy;
+    G_rgb(1,1,1);
+    G_line(p1[X],p1[Y], ci[X], ci[Y]);
 
-  double xpos = pos; double ypos = M*xpos + B;
-  double xneg = neg; double yneg = M*xneg + B;
+    f1(u+0.001,adj);
+    D3d_mat_mult_pt(adj,mat[i],adj);
 
- 
-  if(pow(xpos-p1[0],2) + pow(ypos-p1[1],2) < pow(xneg-p1[0],2) + pow(yneg-p1[1],2)){
-    p1[0] = xpos;
-    p1[1] = ypos;
-  } else {
-    p1[0] = xneg;
-    p1[1] = yneg;
+    G_rgb(1,.4,1);
+    G_line(ci[X],ci[Y],
+           ci[X] + 2000*(adj[Y] - ci[Y]),
+           ci[Y] - 2000*(adj[X] - ci[X])
+           );
   }
-  double u = atan2(p1[1],p1[0]);
-
-  D3d_mat_mult_pt(p1,mat,p1);
-  G_rgb(1,1,1);
-  G_fill_circle(p1[0],p1[1],3);
-
-  f1(u+0.001,p2);
-  D3d_mat_mult_pt(p2,mat,p2);
-  G_rgb(1,0,0);
-  G_fill_circle(p2[0],p2[1],3);
-
-  a = p2[0] - p1[0];
-  b = p2[1] - p1[1];
-
-  xx = p1[0] + 2000*b;
-  yy = p1[1] - 2000*a;
-
-  G_rgb(1,1,1);
-  G_line(p1[0],p1[1],xx,yy) ;
-  G_rgb(0,1,1);
-  G_line(x_old,y_old, p1[0],p1[1]);
-
-  G_wait_key();
+  return 1;
 }
 
 
