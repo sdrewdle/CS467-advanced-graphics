c5992b552a50d0202ec1dd70afe9e2367370468a
sdrewdle <sdrew@lclark.edu>
2018-04-12 17:11:10 -0700

    light model works with hyperboloids

--- ray_tracing/3d_ray_tracing.c
+++ ray_tracing/3d_ray_tracing.c
@@ -1,6 +1,5 @@
 #include <FPT.h>
 #include <D3d_matrix.h>
-#include <parametric_descriptions.h>
 #include <xwd_tools.h>
 //--------------------------------------------general display variables:
 double half_angle = 25 *M_PI/180;
@@ -12,6 +11,9 @@ double half_angle = 25 *M_PI/180;
 #define Y 1
 #define Z 2
 
+#define SPHERE 0
+#define HYPERB 1
+
 #define MMN 20 //maximum number of shapes
 int MN = 0;
 //--------------------------------------lighting and viewpoint variables
@@ -19,10 +21,10 @@ int MN = 0;
 #define DIFFUSE_MAX 0.5
 #define SPEC_POW 30
 
-double zbuff[WW][WH];
 double eye[3], ls[3];
 double coi[3], up[3];
 double VIEW[4][4], iVIEW[4][4];
+double light_in_eye_space[3];
 
 // scale: used for translating 3d --> 2d screen
 void scale(double *v, double scale, int l) {
@@ -31,6 +33,113 @@ void scale(double *v, double scale, int l) {
     v[i] *= scale;
   }
 }
+
+
+int Light_Model (double irgb[3],
+                 double s[3],
+                 double p[3],
+                 double n[3],
+                 double argb[3])
+// s,p,n in eyespace
+
+// irgb == inherent color of object (input to this function)
+// s = location of start of ray (probably the eye)
+// p = point on object (input to this function)
+// n = normal to the object at p (input to this function)
+// argb == actual color of object (output of this function)
+// globals : AMBIENT, MAX_DIFFUSE, SPECPOW, light_in_eye_space[3]
+
+// return 1 if successful, 0 if error
+{
+
+  double len ;
+  double N[3] ; 
+  len = sqrt(n[0]*n[0] + n[1]*n[1] + n[2]*n[2]) ;
+  if (len == 0) return 0 ;
+  N[0] = n[0]/len ;  N[1] = n[1]/len ;  N[2] = n[2]/len ;
+
+  double E[3] ;
+  E[0] = s[0] - p[0] ; 
+  E[1] = s[1] - p[1] ; 
+  E[2] = s[2] - p[2] ; 
+  len = sqrt(E[0]*E[0] + E[1]*E[1] + E[2]*E[2]) ;
+  if (len == 0) return 0 ;
+  E[0] /= len ;  E[1] /= len ;  E[2] /= len ;
+  double NdotE = N[0]*E[0] + N[1]*E[1] + N[2]*E[2] ;
+
+  double L[3] ;
+  L[0] = light_in_eye_space[0] - p[0] ; 
+  L[1] = light_in_eye_space[1] - p[1] ; 
+  L[2] = light_in_eye_space[2] - p[2] ; 
+  len = sqrt(L[0]*L[0] + L[1]*L[1] + L[2]*L[2]) ;
+  if (len == 0) return 0 ;
+  L[0] /= len ;  L[1] /= len ;  L[2] /= len ;
+  double NdotL = N[0]*L[0] + N[1]*L[1] + N[2]*L[2] ;
+
+
+
+
+
+  double max_ambient_and_diffuse = AMBIENT + DIFFUSE_MAX;
+     // this needs to occur BEFORE you possibly jump to LLL below
+
+
+
+
+  double intensity ;
+  if (NdotL*NdotE < 0) {
+    // eye and light are on opposite sides of polygon
+    intensity = AMBIENT ; 
+    goto LLL ;
+  } else if ((NdotL < 0) && (NdotE < 0)) {
+    // eye and light on same side but normal pointing "wrong" way
+    N[0] *= (-1.0) ;    N[1] *= (-1.0) ;    N[2] *= (-1.0) ; 
+    NdotL *= (-1.0) ;
+    NdotE *= (-1.0) ;   // don't use NdotE below, probably should eliminate this
+  }
+
+
+  // ignore Blinn's variant
+  double R[3] ; // Reflection vector of incoming light
+  R[0] = 2*NdotL*N[0] - L[0] ;
+  R[1] = 2*NdotL*N[1] - L[1] ;
+  R[2] = 2*NdotL*N[2] - L[2] ;
+
+  double EdotR = E[0]*R[0] + E[1]*R[1] + E[2]*R[2] ;
+
+  double diffuse ;
+  if (NdotL <= 0.0) { diffuse = 0.0 ; }
+  else { diffuse = DIFFUSE_MAX*NdotL ; }
+
+  double specular ;
+  if (EdotR <= 0.0) { specular = 0.0 ; }
+  else { specular = (1.0 - max_ambient_and_diffuse)*pow(EdotR,SPEC_POW) ;}
+
+  // printf("%lf %lf\n",diffuse,specular) ;
+  intensity = AMBIENT + diffuse + specular ;
+
+
+
+ LLL : ;
+
+  double f,g ;
+  if (intensity <= max_ambient_and_diffuse) {
+    f = intensity / max_ambient_and_diffuse ;
+    argb[0] = f * irgb[0] ;
+    argb[1] = f * irgb[1] ;
+    argb[2] = f * irgb[2] ;
+  } else {
+    f = (intensity - max_ambient_and_diffuse) / 
+                           (1.0 - max_ambient_and_diffuse) ;
+    g = 1.0 - f ;
+    argb[0] = g * irgb[0] + f ;
+    argb[1] = g * irgb[1] + f ;
+    argb[2] = g * irgb[2] + f ;
+  }
+
+  return 1 ;
+}
+
 
 // reflection over a normal vector
 // guaranteed |r| = 1
@@ -42,121 +151,26 @@ void reflection(double l[3], double n[3], double r[3]) {
   normalize(r,3);
 }
 
-void shade(double rgb[3], double sum, double intensity) {
+void shade(double rgb[3], double sum, double intensity,
+           double fin[3]) {
   double ratio;
-  double r = rgb[0], g = rgb[1], b = rgb[2];
-  
-  if (intensity <= sum) {
-    ratio = intensity/sum;
-    G_rgb(r * ratio,
-	  g * ratio,
-	  b * ratio);
-  } else {
-    ratio = (intensity-sum)/(1-sum);
-    G_rgb((1-ratio)*r + ratio,
-	  (1-ratio)*g + ratio,
-	  (1-ratio)*b + ratio);
-  }
-}
-
-// it's light, but with a checkerboard pattern
-void checker(double rgb1[3], double rgb2[3],
-             double sum, double intensity, double u, double v,
-             double rgb[3])
-{
-  v += M_PI/2;
-  double ratio, r, g, b;
-  if((int)(u*100/30) % 2 == 0) {
-    if((int)(v*100/30) % 2 == 0){
-      r = rgb1[0]; g = rgb1[1]; b = rgb1[2];
-    } else {
-      r = rgb2[0]; g = rgb2[1]; b = rgb2[2];
-    }
-  } else {
-    if((int)(v*100/20) % 2 == 1){
-      r = rgb1[0]; g = rgb1[1]; b = rgb1[2];
-    } else {
-      r = rgb2[0]; g = rgb2[1]; b = rgb2[2];
-    }
-  }
-
-  if (intensity <= sum) {
-    ratio = intensity/sum;
-    rgb[0] = r * ratio;
-    rgb[1] = g * ratio;
-    rgb[2] = b * ratio;
-
-  } else {
-    ratio = (intensity-sum)/(1-sum);
-    rgb[0] = (1-ratio)*r + ratio;
-    rgb[1] = (1-ratio)*g + ratio;
-    rgb[2] = (1-ratio)*b + ratio;
-  }
-}
-
-/* sets the rgb value to the appropriate color
- * u,v are the current angles
- * rgb is an array of the base color
- * func is the same function used to get the point;
- *** used to approx the norm
- */
-void light(double u, double v, double mat[4][4],
-           double RGB[3],
-           int (*func)(double uu, double vv, double xyz[3]),
-           double rgb[3])
-{
   int i;
-
-  //-------------------------------------------------define the normal vector
-  double norm[3];
-  double a[3], b[3], c[3];
-  func(u,v,b);         D3d_mat_mult_pt(b,mat,b); //point on vertex
-  func(u+0.01,v,a);   D3d_mat_mult_pt(a,mat,a);
-  func(u,v+0.01,c);   D3d_mat_mult_pt(c,mat,c);
-  normal(a,b,c,norm);
-
-  //------------------------define the eye vector and the light source vector
-  double e[3], l[3];
-  vector_to(eye,b,e); vector_to(ls,b,l);
-  normalize(e,3);
-  normalize(l,3);
-  //------if the ls and eye vectors are obtuse to the normal, flip the normal
-  if(dot(e,norm)<=0 && dot(l,norm)<=0) {
-    for (i=0;i<3;i++) {norm[i] = -norm[i];}
-  }
-  //--------------------------------------------------calculate the intensity
-  double specular, diffuse;
-  
-  double r[3];
-  reflection(l,norm,r);
-  
-  double edr = dot(e,r);
-  double ndl = dot(norm,l);
-  double nde = dot(norm,e);
-
-  if(ndl*nde <= 0) {edr=0; ndl=0;}
-
-  if(edr <= 0) {
-    specular = 0;
+  if (intensity <= sum) {
+    ratio = intensity/sum;
+    for(i=0;i<3;i++) {
+      fin[i] = rgb[i]*ratio;
+    }
   } else {
-    specular = (1-AMBIENT-DIFFUSE_MAX) * pow(edr, SPEC_POW);
+    ratio = (intensity-sum)/(1-sum);
+    for(i=0;i<3;i++){
+      fin[i] = (1-ratio)*rgb[i] + ratio;
+    }
   }
-
-  if(ndl <= 0) {
-    diffuse = 0;
-  } else {
-    diffuse = DIFFUSE_MAX * ndl;
-  }
-
-  //calculate correct rgb value and call G_rgb to set
-  double white[3] = {1,1,1};
-  checker(RGB,white,AMBIENT+DIFFUSE_MAX, AMBIENT + diffuse + specular, u, v, rgb);
 }
-
 void compute_best_intersection(double mat[MMN][4][4], double imat[MMN][4][4],
-                               int (*f[MMN])(double u, double v, double xyz[3]),
+                               int *shape,
                                double p1[3], double p2[3],
-                               double uvi[3]) {
+                               double xyzi[4]) {
   double a, b, c; // variables of quadratic equation
   double pos[3], neg[3], ltemp;
   double pa[3]; // point a, in simplified space
@@ -164,9 +178,9 @@ void compute_best_intersection(double mat[MMN][4][4], double imat[MMN][4][4],
   double dir[3];// direction vector of equation
   double tn, tp;
 
-  uvi[2] = -1;
+  xyzi[3] = -1;
   double shortest_length = 10000;
-  int i;
+  int i,j;
 
   for(i=0;i<MN; i++) {
     D3d_mat_mult_pt(pa,imat[i],p1);
@@ -179,85 +193,95 @@ void compute_best_intersection(double mat[MMN][4][4], double imat[MMN][4][4],
     if(dir[X] == 0 && dir[Y] == 0 && dir[Z] == 0) {continue;}
 
     // finding parameters of quadratic equation:
-    // at^2 + bt + c = 0
-    a = dir[X]*dir[X] + dir[Y]*dir[Y] + dir[Z]*dir[Z];
-    b = 2 * (pa[X]*dir[X] + pa[Y]*dir[Y] + pa[Z]*dir[Z]);
-    c =  pa[X]*pa[X] + pa[Y]*pa[Y] + pa[Z]*pa[Z] - 1;
-
+    if (shape[i] == SPHERE){ // at^2 + bt + c = 0
+      a = dir[X]*dir[X] + dir[Y]*dir[Y] + dir[Z]*dir[Z];
+      b = 2 * (pa[X]*dir[X] + pa[Y]*dir[Y] + pa[Z]*dir[Z]);
+      c =  pa[X]*pa[X] + pa[Y]*pa[Y] + pa[Z]*pa[Z] - 1;
+    } else if (shape[i] == HYPERB) {
+      a = dir[X]*dir[X] - dir[Y]*dir[Y] + dir[Z]*dir[Z];
+      b = 2 * (pa[X]*dir[X] - pa[Y]*dir[Y] + pa[Z]*dir[Z]);
+      c = pa[X]*pa[X] - pa[Y]*pa[Y] + pa[Z]*pa[Z] - 1;
+    }
     // solving quadratic equation
     tp = (-b + sqrt(b*b - 4*a*c)) / (2 * a);
     tn = (-b - sqrt(b*b - 4*a*c)) / (2 * a);
 
-    //check for nan; means they don't intersect
-    if(isnan(tp) && isnan(tn)){continue;}
+    if(isnan(tp) && isnan(tn)){continue;} //they don't intersect generally
 
-    pos[X] = pa[X] + tp*dir[X];
-    pos[Y] = pa[Y] + tp*dir[Y];
-    pos[Z] = pa[Z] + tp*dir[Z];
+    for(j=0;j<3;j++){
+      pos[j] = pa[j] + tp*dir[j];
+      neg[j] = pa[j] + tn*dir[j];
+    }
 
-    neg[X] = pa[X] + tn*dir[X];
-    neg[Y] = pa[Y] + tn*dir[Y];
-    neg[Z] = pa[Z] + tn*dir[Z];
-
-
-    // back in normal view 
-    D3d_mat_mult_pt(pos,mat[i],pos);
-    D3d_mat_mult_pt(neg,mat[i],neg);
-
-    //test for pos being the shortest
-    ltemp = sqrt(pow(pos[X]-p2[X],2) + pow(pos[Y]-p2[Y],2) + pow(pos[Z]-p2[Z],2) );
-    if(ltemp < shortest_length && tp >= 0){
-      D3d_mat_mult_pt(pos,imat[i],pos);
-      uvi[0] = atan2(pos[Y],pos[X]);
-      uvi[1] = atan2(pos[Z],pos[X]);
-      uvi[2] = i;
-      shortest_length = ltemp;
-
-      D3d_mat_mult_pt(pos,mat[i],pos);
+    if(pos[Y] < 1.5 && pos[Y] > -1.5) { // this is only necessary for hyperboloids,
+                                    // but won't mess up spheres
+      //test for pos being the shortest
+      ltemp = tp;
+      if(ltemp < shortest_length && tp >= 0){ //if shortest and t is positive
+        D3d_mat_mult_pt(pos,mat[i],pos);
+        xyzi[0] = pos[X];
+        xyzi[1] = pos[Y];
+        xyzi[2] = pos[Z];
+        xyzi[3] = i;
+        shortest_length = ltemp;
+      }
     }
 
     //test for neg being the shortest
-    ltemp = sqrt(pow(neg[X]-p2[X],2) + pow(neg[Y]-p2[Y],2) + pow(neg[Z]-p2[Z],2) );
-    if (ltemp < shortest_length && tn >= 0){
-      D3d_mat_mult_pt(neg,imat[i],neg);
-      uvi[0] = atan2(neg[Y],neg[X]);
-      uvi[1] = atan2(neg[Z],neg[X]);
-      uvi[2] = i;
-      shortest_length = ltemp;
-
-      D3d_mat_mult_pt(neg,mat[i],neg);
+    if(neg[Y] < 1.5 && neg[Y] > -1.5) { // still only necessary for hyperboloids
+      ltemp = tn;
+      if (ltemp < shortest_length && tn >= 0){ // if shortest and t is positive
+        D3d_mat_mult_pt(neg,mat[i],neg);
+        xyzi[0] = neg[X];
+        xyzi[1] = neg[Y];
+        xyzi[2] = neg[Z];
+        xyzi[3] = i;
+        shortest_length = ltemp;
+      }
     }
-
   }
 }
 
-int plot_with_inc (int map, int (*func[MMN])(double u, double v, double xyz[3]),
-                   double mat[MMN][4][4], double imat[MMN][4][4]
+int plot (int map, int *shape,
+          double mat[MMN][4][4], double imat[MMN][4][4],
+          double base_rgb[5][3]
 	 )
 {
   int p[2];
   double screen[3] = {0,0,1};
   double xyz[3] = {0,0,0};
   double th = tan(half_angle);
-  double uvi[3] = {0,0,0}; // grr give me python tupling
+  double xyzi[4] = {0,0,0,-1}, shade[3]; // returns
   double origin[3] = {0,0,0};
+  double norm[3];
 
-  int i,j;
+  int i,j, obj;
   for (i=0; i < WW; i++) {
     for(j=0; j < WH; j++) {
       screen[X] = -th + i * (2*th) / WW;
       screen[Y] = -th + j * (2*th) / WH;
       screen[Z] = 1;
 
-      compute_best_intersection(mat, imat, func, origin, screen, uvi);
-      if(uvi[2] == -1) {
+      compute_best_intersection(mat, imat, shape, origin, screen, xyzi);
+      if(xyzi[3] == -1) {
         set_xwd_map_color(map,i,j,BG,BG,BG); // set pixel to background color
         continue;
       }
-      (func[ (int)(uvi[2]) ])(uvi[0], uvi[1], xyz);
 
-      //light(uvi[0],uvi[1],mat[uvi[i]],RGB,func, rgb);
-      set_xwd_map_color(map, i, j, 1, 1, 0.5);
+      obj = xyzi[3];
+
+      //compute partial derivative:
+      double xyz_obj[3]; //xyz in object space
+      D3d_mat_mult_pt(xyz_obj, imat[obj], xyzi);
+
+      double par[3] = {2*xyz_obj[X], 2*xyz_obj[Y], 2*xyz_obj[Z]};
+      if(shape[obj] == HYPERB) {par[1] = -par[1];}
+      norm[0] = imat[obj][0][0]*par[X] + imat[obj][1][0]*par[Y] + imat[obj][2][0]*par[Z];
+      norm[1] = imat[obj][0][1]*par[X] + imat[obj][1][1]*par[Y] + imat[obj][2][1]*par[Z];
+      norm[2] = imat[obj][0][2]*par[X] + imat[obj][1][2]*par[Y] + imat[obj][2][2]*par[Z];
+      Light_Model(base_rgb[obj],origin,xyzi,norm,shade);
+
+      set_xwd_map_color(map, i, j, shade[0], shade[1], shade[2]);
     }
   }
   return 1;
@@ -274,11 +298,12 @@ int main()
 
 
   double mat[MMN][4][4],imat[MMN][4][4] ;
-  int (*f[MMN])(double u, double v, double xyz[3]);
+  int shape[MMN];
+  double reflectivity[MMN];
   //---------------------------------------------------------
   // create VIEW matrix
-  eye[0] = 0;
-  eye[1] = 0;
+  eye[0] = 4;
+  eye[1] = 3;
   eye[2] = -10;
   up[0] = eye[0];
   up[1] = eye[1] + 1;
@@ -287,6 +312,10 @@ int main()
   D3d_make_identity(VIEW); D3d_make_identity(iVIEW);
   D3d_view(VIEW,iVIEW,eye,coi,up);
 
+  ls[X] = 10;
+  ls[Y] = 10;
+  ls[Z] = -5;
+  D3d_mat_mult_pt(light_in_eye_space,VIEW,ls);
   //---------------------------------------------------------
 
   // keep track of matrix to define single shape
@@ -294,24 +323,41 @@ int main()
   double Tvlist[100] ;
 
   //---------------------------------------------------------
-  // sphere1 (inc by 0.005)
+  // hyperboloid
   Tn = 0 ;
-  //Ttypelist[Tn] = TX ; Tvlist[Tn] =  4 ; Tn++ ;
+  Ttypelist[Tn] = TY ; Tvlist[Tn] = -1 ; Tn++;
+  D3d_make_movement_sequence_matrix (mat[MN],imat[MN],
+                                     Tn,
+                                     Ttypelist,
+                                     Tvlist) ;
+  reflectivity[MN] = 0;
+  shape[MN] = HYPERB; MN++;
+  //---------------------------------------------------------
+  // sphere1
+  Tn = 0 ;
+  Ttypelist[Tn] = TX ; Tvlist[Tn] =  1.1 ; Tn++ ;
+  Ttypelist[Tn] = TY ; Tvlist[Tn] = 2    ; Tn++ ;
 
   D3d_make_movement_sequence_matrix (mat[MN],imat[MN],
                                      Tn,
                                      Ttypelist,
                                      Tvlist) ;
-  f[MN] = sphere; MN++;
-
+  reflectivity[MN] = 1;
+  shape[MN] = SPHERE; MN++;
 
   int i;
   for(i = 0; i < MN; i++){
     D3d_mat_mult(mat[i], mat[i],VIEW);
-    D3d_mat_mult(imat[i], imat[i],iVIEW);
+    D3d_mat_mult(imat[i], iVIEW,imat[i]);
   }
 
-  plot_with_inc(map,f,mat,imat);
+  double rgb[5][3] = {{1,1,0.5},
+                      {0.5,0.1,0.6},
+                      {1,0,0},
+                      {0,1,0},
+                      {0,0,1}};
+
+  plot(map,shape,mat,imat,rgb);
   xwd_map_to_named_xwd_file(map, filename);
 
   return 1;
